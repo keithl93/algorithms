3bbc4d1b7ef1aa7abae8e5c6e7abbedb6d36277a
Keith Lee <keithfromseoul@gmail.com>
2023-08-08 19:55:51 -0400

    commit

---
+++ mergeSort/study.js
@@ -0,0 +1,39 @@
+// helper function
+function merge(arr1, arr2) {
+  const results = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.sllice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+console.log(merge([14, 22, 78, 54, 74, 1, 10]));
