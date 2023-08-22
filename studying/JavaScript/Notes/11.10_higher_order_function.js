8550a6cd24d85f34ea62af800df7c572f839dd30
Keith Lee <khlee930121@gmail.com>
2023-08-21 23:45:37 -0400

    study

---
+++ studying/JavaScript/Notes/11.10_higher_order_function.js
@@ -0,0 +1,8 @@
+const fruits = ['🍌', '🍐', '🍊', '🍓'];
+//for 문 대신 하나씩 list하는걸 편하게 하는 고차함수
+//배열을 빙글빙글 돌면서 원하는것을 하고싶을때
+// fruits.forEach(function (value, index, array) {
+//   console.log(value);
+// });
+
+fruits.forEach((something) => console.log(something));
